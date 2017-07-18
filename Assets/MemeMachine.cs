1a62974b3216282f4364b7b65ae7d7c0a11fb39d
Myles Sloan <mylessloansf@gmail.com>
2017-07-18 16:19:46 -0700

    idk even xD

---
+++ Assets/MemeMachine.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MemeMachine : MonoBehaviour {
+
+    public int doge;
+
+	// Use this for initialization
+	void Start ()
+    {
+        doge += 5;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/NewBehaviourScript.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NewBehaviourScript : MonoBehaviour
+{
+    public int Memes = 0;
+    public float MemesPerSecond = 0;
+    public int ClickPower = 1;
+    public Text currentMemes;
+    public Text memesPerSecond;
+
+    public float timer;
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+
+    // Update is called once per frame
+    void Update()
+    {
+        currentMemes.text = Memes.ToString() + " Memes";
+        memesPerSecond.text = MemesPerSecond.ToString() + " Memes Per Second";
+
+        timer += 1 * Time.deltaTime;
+        //Calculates Memes Per Second
+        MemesPerSecond = Memes / timer;
+        /*
+        if (Input.GetKeyUp(KeyCode.Mouse0)) { 
+        MemesPerSecond = 1; }
+        */
+       }
+    
+    public void GUI()
+    {
+
+         }
+    public void Doge()
+        //Code for adding 
+    { Memes = Memes + ClickPower;
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
